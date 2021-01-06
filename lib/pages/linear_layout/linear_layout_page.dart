e141dd2cf54f020a749c1b968965106e9867bb96
Monster <monster@admindeMacBook-Pro.local>
2021-01-06 09:30:25 +0800

    init linear layout page

---
+++ lib/pages/linear_layout/linear_layout_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LinearLayoutPage extends StatefulWidget {
+  LinearLayoutPage({Key key}) : super(key: key);
+
+  @override
+  _LinearLayoutPageState createState() => _LinearLayoutPageState();
+}
+
+class _LinearLayoutPageState extends State<LinearLayoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Linear Layout Page...'),
+    );
+  }
+}
